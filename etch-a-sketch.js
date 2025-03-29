3518ba7c0f2e56175327cf3d5f97cd7897953966
KaitoMikazuki <175144170+KaitoMikazuki@users.noreply.github.com>
2025-03-29 19:40:17 +0800

    Sketching funtionality on container added

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,18 +1,31 @@
 function main(){
-    
+    let gridSize = 100;
+    // Clones instead of building row from scratch
+    for (let i = 0; i < gridSize; i++){
+        let row = createRow(gridSize);
+        document.querySelector(".container").append(row)
+    }
 }
 
+function createRow(gridSize){
+    let row = document.createElement("div")
+    row.classList.add("flex-row");
+    for (let i = 0; i < gridSize; i++){
+        row.append(createSquare());
+        console.log("square created");
+    }
+    return row
+}
 function createSquare(){
     let square = document.createElement("div");
-    square.classList.add(flex-squares);
+    square.classList.add("flex-squares");
+    square.addEventListener('mouseenter', drawOnSquare)
     return square;
 }
 
-function createRow(gridWidth = 16){
-    let row = document.createElement("div")
-    for (int i = 0; i < gridWidth; i++;){
-        row.append(createSquare());
-    }
+function drawOnSquare (event){
+    event.target.style.backgroundColor = "black"
 }
+
 
 main();
